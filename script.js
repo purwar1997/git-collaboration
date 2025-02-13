633331bc93c64af55c67f796f86673d80288354d
purwar1997 <shubhampurwar511@gmail.com>
2025-02-13 12:35:06 +0530

    Change heading text when a button is clicked

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const heading = document.querySelector('.main-heading');
 const button = document.querySelector('.click-btn');
 
 button.addEventListener('click', function () {
   console.log('Button clicked');
+  heading.textContent = 'Livebench platform is now online';
 });
